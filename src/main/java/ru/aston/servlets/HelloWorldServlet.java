80e4e75f2b7ee634159f556a5858a1c2389e2019
greaterr <greaterr@student.21-school.ru>
2024-09-16 17:03:47 +0500

    Servlet_fixed

--- src/main/java/ru/aston/servlets/HelloWorldServlet.java
+++ src/main/java/ru/aston/servlets/HelloWorldServlet.java
@@ -9,7 +9,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet("/webapp")
+@WebServlet("/")
 public class HelloWorldServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
