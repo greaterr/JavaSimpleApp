3ea1194caf2a6dd124a8f419c96d111b83b76bd3
greaterr <greaterr@student.21-school.ru>
2024-09-16 17:16:45 +0500

    Servlet_fixed_3

--- src/main/java/ru/aston/servlets/HelloWorldServlet.java
+++ src/main/java/ru/aston/servlets/HelloWorldServlet.java
@@ -1,7 +1,6 @@
 package ru.aston.servlets;
 
 import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -9,7 +8,6 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet("/")
 public class HelloWorldServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
